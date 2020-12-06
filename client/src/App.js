a7a005ceda0b815c4890685339e2537631fe612f
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2020-12-05 20:36:20 -0500

    top tracks display

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,31 @@ class App extends Component {
     topTracks: [],
   };
 
+  componentDidMount(){
+    this.getTracks()
+  }
+
   getTracks = () => {
     API.getTopTracks(this.state.topTracks).then((res) => {
       this.setState({ topTracks: res.data.tracks.track });
     });
   };
 
-
- 
-  
+  likeTracks = () => {
+    console.log( this.state.track.artist.name + "Clicked")
+  }
 
 
   render() {
    
-    console.log(this.state.topTracks[0]?.artist?.name);
+    console.log(this.state.topTracks);
 
     return (
       <div className="App">
         <button onClick={this.getTracks}>click</button>
         {this.state.topTracks.map((track) => {
           {
-           return <h2>{track.artist.name}</h2>;
+          return <h2>{track.artist.name} - {track.name} <button onClick = {this.likeTracks}>LIKE</button></h2>
           }
         })}
       </div>
