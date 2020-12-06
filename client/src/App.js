3526e4c3f6305f4c3de416314666e15ad9066948
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2020-12-05 19:19:22 -0500

    get tracks

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,34 @@ import "./App.css";
 import React, { Component } from "react";
 import API from "./utils/API";
 
-
 class App extends Component {
   state = {
     topTracks: [],
   };
 
-
   getTracks = () => {
-    API.getTopTracks(this.state.topTracks).then(res => {
-      this.setState({ topTracks: res.data.tracks.track});
-    })
+    API.getTopTracks(this.state.topTracks).then((res) => {
+      this.setState({ topTracks: res.data.tracks.track });
+    });
   };
-  
-  
 
-  // createTopTrack = () => {
-  //   let res = topTracksApi.post("/", {
-  //     topTracks
-  //   });
-  //   console.log(res);
-  //   this.getTracks();
-  // };
+
+ 
+  
 
 
   render() {
-    console.log(this.state.topTracks)
+   
+    console.log(this.state.topTracks[0]?.artist?.name);
+
     return (
       <div className="App">
         <button onClick={this.getTracks}>click</button>
-        {/* <h2>{[this.state.topTracks[0]]}</h2> */}
+        {this.state.topTracks.map((track) => {
+          {
+           return <h2>{track.artist.name}</h2>;
+          }
+        })}
       </div>
     );
   }
